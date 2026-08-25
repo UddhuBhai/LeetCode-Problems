751dd1825b48f221c8f4eaae9b584dd6fd9542e4
Uddhav Matholia <uddhavmatholia2712007@gmail.com>
2026-08-25 18:42:39 +0530

    Add files via upload

---
+++ #13 - "Roman To Integer"/#13 - Roman To Integer.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> values = {
+            {'I', 1}, {'V', 5}, {'X', 10},
+            {'L', 50}, {'C', 100},
+            {'D', 500}, {'M', 1000}
+        };
+
+        int result = 0;
+
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (values[s[i]] < values[s[i + 1]]) {
+                result -= values[s[i]];
+            } else {
+                result += values[s[i]];
+            }
+        }
+
+        result += values[s[s.length() - 1]];
+
+        return result;
+    }
+};
